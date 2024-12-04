e2e62b3a23d8d0ed8ffdb64bce96ba59db7c6c86
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2024-12-04 00:17:34 -0300

    style: add animation to Skills component

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import css from '@/assets/css3.svg'
 import html from '@/assets/html5.svg'
 import javascript from '@/assets/javascript.svg'
@@ -11,11 +13,26 @@ import sass from '@/assets/sass.svg'
 import node from '@/assets/node.svg'
 import prisma from '@/assets/prisma.svg'
 
+import { motion, useInView } from 'motion/react'
+
 import { SkillCard } from './skill-card'
+import { useRef } from 'react'
 
 export function Skills() {
+  const ref = useRef(null)
+  const isInView = useInView(ref)
+
   return (
-    <section className="mx-auto my-10 max-w-6xl px-4">
+    <motion.section
+      ref={ref}
+      className="mx-auto my-10 max-w-6xl px-4"
+      initial={{ x: '-10%', opacity: 0 }}
+      animate={{
+        x: isInView ? '0%' : '-10%',
+        opacity: isInView ? 1 : 0,
+      }}
+      transition={{ ease: 'easeOut' }}
+    >
       <h2 className="mb-10 text-center text-3xl font-bold">Conhecimentos</h2>
       <div className="grid grid-cols-[repeat(auto-fill,120px)] justify-center gap-10">
         <SkillCard name="HTML5" source={html} alt="HTML" />
@@ -35,6 +52,6 @@ export function Skills() {
         <SkillCard name="Node.js" source={node} alt="Node.js" />
         <SkillCard name="Prisma" source={prisma} alt="Prisma" />
       </div>
-    </section>
+    </motion.section>
   )
 }
