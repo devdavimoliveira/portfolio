388ee8b949403910966e42f398ad8641b07f2ecb
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2024-12-05 17:50:05 -0300

    feat: create AboutMe component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from '@/components/hero'
+import { AboutMe } from '@/components/about-me'
 import { Skills } from '@/components/skills'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <AboutMe />
       <Skills />
     </main>
   )
---
+++ src/components/about-me.tsx
@@ -0,0 +1,13 @@
+export function AboutMe() {
+  return (
+    <section className="mx-4 max-w-6xl border-b border-green-600 min-[1152px]:mx-auto">
+      <h2 className="mb-5 text-center text-3xl font-bold">Sobre mim</h2>
+      <p className="mx-auto mb-10 w-full text-center text-lg md:w-[58%]">
+        Desenvolvedor Front-end com experiência em React.js, Next.js JavaScript,
+        TypeScript, Styled-Components, TailwindCSS e consumo de APIs REST.
+        Também possuo um pouco de experiência no Back-end com NestJS e Prisma
+        ORM. Formado em Sistemas de Informação.
+      </p>
+    </section>
+  )
+}
