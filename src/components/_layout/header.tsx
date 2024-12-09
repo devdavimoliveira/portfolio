7e2b6407cae10b80756d5820de2c60b8b6fa715e
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2024-12-09 16:52:11 -0300

    feat: create OverlayMenu component

--- src/components/_layout/header.tsx
+++ src/components/_layout/header.tsx
@@ -1,9 +1,19 @@
-import { Menu } from 'lucide-react'
+'use client'
+
+import { Menu as MenuHamburger, X } from 'lucide-react'
+import { useState } from 'react'
+import { OverlayMenu } from './overlay-menu'
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  function toggleMenu() {
+    setMenuOpen((prevState) => !prevState)
+  }
+
   return (
-    <header className="shadow-sm shadow-green-600/10">
-      <nav className="mx-auto flex h-20 max-w-6xl items-center justify-between px-4">
+    <header className="sticky top-0 z-50 bg-zinc-900 shadow-sm shadow-green-600/10">
+      <div className="mx-auto flex h-20 max-w-6xl items-center justify-between px-4">
         <div>
           <span className="text-3xl font-bold uppercase text-green-600">
             DEV.
@@ -11,10 +21,16 @@ export function Header() {
           <span className="text-2xl">davimoliveira</span>
         </div>
 
-        <button>
-          <Menu size={32} className="hover:text-gray-200" />
+        <button onClick={toggleMenu}>
+          {menuOpen ? (
+            <X size={32} className="hover:text-gray-200" />
+          ) : (
+            <MenuHamburger size={32} className="hover:text-gray-200" />
+          )}
         </button>
-      </nav>
+      </div>
+
+      <OverlayMenu isOpen={menuOpen} onOpenChange={setMenuOpen} />
     </header>
   )
 }
---
+++ src/components/_layout/overlay-menu.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import { useEffect } from 'react'
+import Link from 'next/link'
+
+import { AnimatePresence, motion } from 'motion/react'
+
+type OverlayMenuProps = {
+  isOpen: boolean
+  onOpenChange: (state: boolean) => void
+}
+
+export function OverlayMenu({ isOpen, onOpenChange }: OverlayMenuProps) {
+  useEffect(() => {
+    const body = document.querySelector('body')
+
+    if (body) {
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth
+
+      if (isOpen) {
+        body.style.paddingRight = `${scrollbarWidth}px`
+        body.style.overflow = 'hidden'
+      }
+
+      return () => {
+        body.style.paddingRight = '0px'
+        document.body.style.overflow = 'visible'
+      }
+    }
+  }, [isOpen])
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.nav
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="absolute h-screen w-full bg-zinc-900/50 backdrop-blur-md"
+          onClick={() => onOpenChange(false)}
+        >
+          <div className="mx-auto h-[200px] max-w-6xl px-4">
+            <ul className="mt-20 flex flex-col items-center gap-16">
+              <li>
+                <Link
+                  href="#inicio"
+                  className="text-3xl font-bold transition-colors duration-500 hover:text-green-600"
+                >
+                  Início
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#sobre-mim"
+                  className="text-3xl font-bold transition-colors duration-500 hover:text-green-600"
+                >
+                  Sobre mim
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#conhecimentos"
+                  className="text-3xl font-bold transition-colors duration-500 hover:text-green-600"
+                >
+                  Conhecimentos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#projetos"
+                  className="text-3xl font-bold transition-colors duration-500 hover:text-green-600"
+                >
+                  Projetos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#contato"
+                  className="text-3xl font-bold transition-colors duration-500 hover:text-green-600"
+                >
+                  Contato
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </motion.nav>
+      )}
+    </AnimatePresence>
+  )
+}
