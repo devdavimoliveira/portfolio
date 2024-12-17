9b2023cad8497b194a31f6e7198927e2f731f6a0
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2024-12-17 14:21:14 -0300

    feat: add person structured data (JSON-LD)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,21 @@ import { roboto } from '@/styles/fonts'
 import { Header } from '@/components/_layout/header'
 import { Footer } from '@/components/_layout/footer'
 
+import { WithContext, Person } from 'schema-dts'
+
 export const metadata: Metadata = {
   title: 'Portfólio | Davi Machado Oliveira',
   description:
     'Meu nome é Davi, desenvolvo sites com interfaces modernas com foco em desempenho, e que entregam uma boa experiência para o usuário.',
+}
+
+const jsonLd: WithContext<Person> = {
+  '@context': 'https://schema.org',
+  '@type': 'Person',
+  name: 'Davi Machado Oliveira',
+  jobTitle: 'Desenvolvedor Web',
+  url: process.env.WEBSITE_URL,
+  sameAs: ['https://www.linkedin.com/in/devdavimoliveira'],
 }
 
 export default function RootLayout({
@@ -20,6 +31,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body className={`${roboto.variable} antialiased`}>
         <Header />
         {children}
